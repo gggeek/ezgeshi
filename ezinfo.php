71fe7528083da9a8e501f7c8a9198201b5f5edf1
Cyberwolf <Cyberwolf@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2007-01-04 10:35:00 +0000

    extension info file for eZ publish 3.9
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1406 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

---
+++ ezinfo.php
@@ -0,0 +1,19 @@
+<?php
+
+class ezshInfo
+{
+    function info()
+    {
+        return array( 'Name' => "eZ Syntax Highlighter",
+                      'Version' => "1.x",
+                      'Copyright' => "Copyright (C) 2005-2006 Lukasz Serwatka",
+                      'License' => "GNU General Public License v2.0",
+                      'Includes the following third-party software' => array( 'Name' => 'GeSHi - Generic Syntax Highlighter',
+                                                                              'Version' => '1.0.7.16',
+                                                                              'License' => 'GNU General Public License v2.0',
+                                                                              'For more information' => 'http://qbnz.com/highlighter/' )
+                      );
+    }
+}
+
+?>
