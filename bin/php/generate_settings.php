5787e925388277a21ab21bdce4e97ed3174199bb
Cyberwolf <Cyberwolf@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2007-05-12 08:34:42 +0000

    - Added script to generate class settings for content.ini based on the GeSHi language files - Updated class settings
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1616 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

---
+++ bin/php/generate_settings.php
@@ -0,0 +1,70 @@
+#!/usr/bin/env php
+<?php
+
+include_once( 'kernel/classes/ezscript.php' );
+include_once( 'lib/ezutils/classes/ezcli.php' );
+include_once( 'extension/ezsh/lib/geshi/geshi.php' );
+
+$cli =& eZCLI::instance();
+
+$scriptSettings = array();
+$scriptSettings['description'] = 'Generate content settings from the GeSHi language files';
+$scriptSettings['use-session'] = true;
+$scriptSettings['use-modules'] = true;
+$scriptSettings['use-extensions'] = true;
+
+$script =& eZScript::instance( $scriptSettings );
+$script->startup();
+
+$config = '';
+$argumentConfig = '';
+$optionHelp = false;
+$arguments = false;
+$useStandardOptions = true;
+
+$options = $script->getOptions( $config, $argumentConfig, $optionHelp, $arguments, $useStandardOptions );
+$script->initialize();
+
+$languageDirPath = 'extension/ezsh/lib/geshi/geshi';
+$handle = opendir( $languageDirPath );
+
+$langs = array();
+
+while ( false !== ( $file = readdir( $handle ) ) )
+{
+    if ( $file == '.' || $file == '..' )
+    {
+        continue;
+    }
+
+    //$cli->output( $file );
+
+    $languageFilePath = $languageDirPath . '/' . $file;
+
+    $language_data = array();
+    include( $languageFilePath );
+    if ( isset( $language_data['LANG_NAME'] ) )
+    {
+        $langIdentifier = basename( $file, '.php' );
+        $langs[$langIdentifier] = $language_data['LANG_NAME'];
+    }
+}
+
+natcasesort( $langs );
+
+$cli->output( '[literal]' );
+foreach ( $langs as $identifier => $name )
+{
+    $cli->output( 'AvailableClasses[]=' . $identifier );
+}
+
+$cli->output( '' );
+
+foreach ( $langs as $identifier => $name )
+{
+    $cli->output( 'ClassDescription[' . $identifier . ']=' . $name );
+}
+
+$script->shutdown( 0 );
+
+?>
