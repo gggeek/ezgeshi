9fb1d30338fd0082c2c8219084e3bba9342f9b94
gg <gg@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2008-03-06 11:23:39 +0000

    - Updated extension info
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1618 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

--- ezinfo.php
+++ ezinfo.php
@@ -4,12 +4,12 @@ class ezshInfo
 {
     function info()
     {
-        return array( 'Name' => "eZ Syntax Highlighter",
-                      'Version' => "1.x",
+        return array( 'Name' => "eZ Geshi (aka eZ Syntax Highlighter)",
+                      'Version' => "1.2",
                       'Copyright' => "Copyright (C) 2005-2006 Lukasz Serwatka",
                       'License' => "GNU General Public License v2.0",
                       'Includes the following third-party software' => array( 'Name' => 'GeSHi - Generic Syntax Highlighter',
-                                                                              'Version' => '1.0.7.19',
+                                                                              'Version' => '1.0.7.20',
                                                                               'License' => 'GNU General Public License v2.0',
                                                                               'For more information' => 'http://qbnz.com/highlighter/' )
                       );
