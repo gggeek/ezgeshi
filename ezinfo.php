bd37538779fcf360e10fa32024f7e3140f061db4
gg <gg@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2008-12-22 21:36:05 +0000

    - update changelog, version info for release 1.2.1
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1640 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ezshInfo
     static function info()
     {
         return array( 'Name' => "eZ Geshi (a.k.a. eZSH, a.k.a. eZ Syntax Highlighter)",
-                      'Version' => "1.2",
+                      'Version' => "1.2.1",
                       'Copyright' => "Copyright (C) 2005-2006 Lukasz Serwatka",
                       'License' => "GNU General Public License v2.0",
                       'Includes the following third-party software' => array( 'Name' => 'GeSHi - Generic Syntax Highlighter',
