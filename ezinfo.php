b6c757004e65c209214d3d33acf4a0473e6576ca
gg <gg@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2010-11-02 22:47:29 +0000

    - fix ezsh tpl operator (was broken in 1.3 release)
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1667 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ezshInfo
     static function info()
     {
         return array( 'Name' => "eZ Geshi (a.k.a. eZSH, a.k.a. eZ Syntax Highlighter)",
-                      'Version' => "1.3",
+                      'Version' => "1.3.1",
                       'Copyright' => "Copyright (C) 2005-2010 Lukasz Serwatka",
                       'License' => "GNU General Public License v2.0",
                       'Includes the following third-party software' => array( 'Name' => 'GeSHi - Generic Syntax Highlighter',
