0a179779b7344a36c1d53bb833de86bd3309aed4
gg <gg@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2012-03-13 17:24:58 +0000

    Add more comments stating geshi appartenance
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1672 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

--- lib/geshi/geshi/eztemplate.php
+++ lib/geshi/geshi/eztemplate.php
@@ -35,6 +35,24 @@
  *  -  fix link for keywords that are different depending on context (eg. break, case)
  *  -  color string literals used without quotes within hashes/arrays
  *  -  the literal tags temselves should not shown in black
+ *************************************************************************************
+ *
+ *     This file is part of GeSHi.
+ *
+ *   GeSHi is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   GeSHi is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with GeSHi; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  ************************************************************************************/
 
 $language_data = array (
