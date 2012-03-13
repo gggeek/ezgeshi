eb5f56846e7bf2022ce310c711c8d8a175691c91
gg <gg@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2012-03-13 15:37:46 +0000

    Upgrade to geshi 1.8.10 Upgrade tags to release 1.4
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1669 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

--- ezinfo.php
+++ ezinfo.php
@@ -5,11 +5,11 @@ class ezshInfo
     static function info()
     {
         return array( 'Name' => "eZ Geshi (a.k.a. eZSH, a.k.a. eZ Syntax Highlighter)",
-                      'Version' => "1.3.1",
-                      'Copyright' => "Copyright (C) 2005-2010 Lukasz Serwatka",
+                      'Version' => "1.4",
+                      'Copyright' => "Copyright (C) 2005-2012 Lukasz Serwatka",
                       'License' => "GNU General Public License v2.0",
                       'Includes the following third-party software' => array( 'Name' => 'GeSHi - Generic Syntax Highlighter',
-                                                                              'Version' => '1.0.8.6',
+                                                                              'Version' => '1.0.8.10',
                                                                               'License' => 'GNU General Public License v2.0',
                                                                               'For more information' => 'http://qbnz.com/highlighter/' )
                       );
