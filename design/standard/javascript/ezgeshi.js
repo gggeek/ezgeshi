7fcefbc9bfdaef39f4168cd91434dc6e3adab7ed
gg <gg@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2012-03-23 02:03:20 +0000

    Take over complete control of debug output: add links to all files
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1679 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

--- design/standard/javascript/ezgeshi.js
+++ design/standard/javascript/ezgeshi.js
@@ -26,7 +26,6 @@
                     $(this).attr( "href", $(this).attr( "href" ).replace( '/visual/templateedit/', '/geshi/highlight/' ) );
                 }
             )
-
             // and make this apparent in the "edit" buttons title in debug table
             $('#debug a[ href*="/geshi/highlight/"] > img').each(
                 function( index )
@@ -38,6 +37,53 @@
             )
             $("#templateusage th:contains('Edit')").html('View');
 
+            // @bug will not work if in non-vhost mode but with no index.php in path
+            var prefix = '';
+            if ( document.location.pathname.indexOf( 'index.php' ) != -1 )
+            {
+                prefix = document.location.pathname.substr( 0, document.location.pathname.indexOf( '/', document.location.pathname.indexOf( 'index.php' ) + 10 ) );
+            }
+
+            // add links to included php files list (from ez 2012.3 / 4.7)
+            // @todo we should remove to remove path to eZ from the link
+            $('#debug_includes td').each(
+                function( index )
+                {
+
+                    if ( $(this).html().substring( 0, 3 ) != '<b>' )
+                    {
+                        $(this).html( '<a href="' + prefix + '/geshi/highlight/' + $(this).html().replace( /\\/g, '/' ) + '">'  + $(this).html() + '</a>' );
+                    }
+                }
+            )
+
+            // add links to css/js files included by ezjscore
+            $('#ezjscpackerusage td:last-child').each(
+                function( index )
+                {
+                    var list = $(this).html().split( /<br>/g );
+                    //alert ( typeof list );
+                    for ( var i = 0; i < list.length; i++ )
+                    {
+                        list[i] = '<a href="' + prefix + '/geshi/highlight/' + list[i].replace( /\\/g, '/' ) + '">'  + list[i] + '</a>'
+                    }
+                    //alert(alist);
+                    $(this).html( list.join( '<br/>' ) );
+                }
+            )
+
+            // last but not least, in error messages that mention tpl files
+            $('#debug table[title="Table for actual debug output, shows notices, warnings and errors"] tr[class="error"]').each(
+                function( index )
+                {
+                    var file = $(this).next().find( 'pre' ).html().match( /@ .+:[0-9]+\[[0-9]+\]/ );
+                    if ( file != null )
+                    {
+                        file = file[0].substr( 2, file[0].indexOf( ':' ) -2 );
+                        msg.html( msg.html().replace( file, '<a href="' + prefix + '/geshi/highlight/' + file + '">' + file + '</a>' ) );
+                    }
+                }
+            )
         }
     );
 
