ee841a514c333722674e341f04851b5d668eefe3
gg <gg@cd7c6d6a-abb1-40a4-9466-a9718d32eec9>
2012-03-23 13:29:02 +0000

    Improve install docs and default settings Improve the debug output monkeypatching
    git-svn-id: http://svn.projects.ez.no/ezgeshi/trunk/extension/ezsh@1680 cd7c6d6a-abb1-40a4-9466-a9718d32eec9

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -4,9 +4,21 @@
 DesignExtensions[]=ezsh
 
 [JavaScriptSettings]
+
+# Uncomment these setting to enable integration of ez geshi with debug output:
+# the debug output results will be enhanced with links to display the
+# source code of all php and template file used.
+# They are left commented out by default to avoid loading unnecessary javascript
+# in production websites
+
+# eZP 4.3.0 and later
 #FrontendJavaScriptList[]=ezjsc::jquery
-FrontendJavaScriptList[]=ezgeshi.js
-#BackendJavaScriptLis[]=ezjsc::jquery
-BackendJavaScriptList[]=ezgeshi.js
+#FrontendJavaScriptList[]=ezgeshi.js
+# (for admin interface design, we can assume jQuery is already loaded)
+#BackendJavaScriptList[]=ezgeshi.js
+
+# eZP 4.2.0 and earlier
+# Make sure you have jQuery loaded, either via ezjscore or some other means
+#JavaScriptList[]=ezgeshi.js
 
 */ ?>
