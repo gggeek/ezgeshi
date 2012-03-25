be213d644ae394262f8a268364f231d5e7eec200
gggeek <gg@ez.no>
2012-03-25 03:32:52 +0200

    Add gitignore Update files with next rel. number

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ezshInfo
     static function info()
     {
         return array( 'Name' => "eZ Geshi (a.k.a. eZSH, a.k.a. eZ Syntax Highlighter)",
-                      'Version' => "1.4-dev",
+                      'Version' => "1.4.1-dev",
                       'Copyright' => "Copyright (C) 2005-2012 Lukasz Serwatka",
                       'License' => "GNU General Public License v2.0",
                       'Includes the following third-party software' => array( 'Name' => 'GeSHi - Generic Syntax Highlighter',
